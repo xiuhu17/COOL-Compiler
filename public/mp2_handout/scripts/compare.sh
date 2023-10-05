b3748edb0dbe5cd449a260a8f8b49e1d217875b6
xiuhu17 <mizhou17@yahoo.com>
2023-10-05 15:57:10 -0500

    update

--- public/mp2_handout/scripts/compare.sh
+++ public/mp2_handout/scripts/compare.sh
@@ -3,6 +3,10 @@
 # 设定路径
 SCRIPT_DIR="../scripts"
 TESTCASE_DIR="../mp2_testcases"
+RESULT_DIR="../results"
+
+# 确保 results 文件夹存在
+mkdir -p $RESULT_DIR
 
 # 获取所有的 .cl 文件数量
 n=$(find $TESTCASE_DIR -name "*.cl" | wc -l)
@@ -19,9 +23,9 @@ for file in *.cl; do
     # 获取文件名（无扩展名）
     base="${file%.*}"
 
-    # 执行生成输出文件的命令
-    make $base.out debug=false
-    make $base.outref
+    # 执行生成输出文件的命令，忽略其输出
+    make $base.out > /dev/null 2>&1
+    make $base.outref > /dev/null 2>&1
 
     # 增加总测试数
     total_tests=$((total_tests+1))
@@ -31,17 +35,17 @@ for file in *.cl; do
     if [ $? -eq 0 ]; then
         # 如果文件相同
         passed_tests=$((passed_tests+1))
-        echo "pass [$total_tests/$n]"
+        echo "pass [$total_tests/$n]" >> $RESULT_DIR/result.txt
     else
-        echo "fail [$total_tests/$n]"
+        echo "fail [$total_tests/$n]" >> $RESULT_DIR/result.txt
     fi
 done
 
 # 输出总结
-echo "Total Pass [$passed_tests/$n]"
+echo "Total Pass [$passed_tests/$n]" >> $RESULT_DIR/result.txt
 
-# 执行 make clean
-make clean
+# 执行 make clean 并忽略其输出
+make clean > /dev/null 2>&1
 
 # 切换回原来的目录
 cd -
---
+++ public/mp2_handout/test/copy.ll
@@ -0,0 +1,38 @@
+declare i32 @strcmp(i8*, i8*)
+declare i32 @printf(i8*, ...)
+declare void @abort()
+declare i8* @malloc(i32)
+define i32 @Main.main() {
+
+entry:
+	%vtpm.0 = alloca i32
+	%vtpm.1 = icmp slt i32 2, 1
+	br i1 %vtpm.1, label %true.0, label %false.0
+
+true.0:
+	store i32 11, i32* %vtpm.0
+	br label %end.0
+
+false.0:
+	store i32 14, i32* %vtpm.0
+	br label %end.0
+
+end.0:
+	%vtpm.2 = load i32, i32* %vtpm.0
+	ret i32 %vtpm.2
+
+abort:
+	call void @abort(  )
+	unreachable
+}
+
+@main.printout.str = internal constant [25 x i8] c"Main.main() returned %d\0A\00"
+define i32 @main() {
+
+entry:
+	%vtpm.4 = call i32 @Main.main(  )
+	%tpm = getelementptr [25 x i8], [25 x i8]* @main.printout.str, i32 0, i32 0
+	%vtpm.5 = call i32(i8*, ... ) @printf( i8* %tpm, i32 %vtpm.4 )
+	ret i32 0
+}
+
