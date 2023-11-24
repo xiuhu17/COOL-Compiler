2a925f8e0fd63466526ef13268096960237f7435
xiuhu17 <mizhou17@yahoo.com>
2023-11-24 11:44:50 -0600

    try

---
+++ public/mp4_handout/llvm15/llvm/lib/CodeGen/RegAllocSimple.cpp
@@ -0,0 +1,137 @@
+//===----------------------------------------------------------------------===//
+//
+/// A register allocator simplified from RegAllocFast.cpp
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/ADT/Statistic.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+#include "llvm/CodeGen/RegAllocRegistry.h"
+#include "llvm/CodeGen/RegisterClassInfo.h"
+#include "llvm/CodeGen/TargetInstrInfo.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
+#include "llvm/InitializePasses.h"
+#include "llvm/Pass.h"
+
+#include "llvm/CodeGen/LiveIntervals.h"
+#include "llvm/CodeGen/SlotIndexes.h"
+
+#include <map>
+#include <set>
+
+using namespace llvm;
+
+#define DEBUG_TYPE "regalloc"
+
+STATISTIC(NumStores, "Number of stores added");
+STATISTIC(NumLoads , "Number of loads added");
+
+namespace {
+  /// This is class where you will implement your register allocator in
+  class RegAllocSimple : public MachineFunctionPass {
+  public:
+    static char ID;
+    RegAllocSimple() : MachineFunctionPass(ID) {}
+
+  private:
+    /// Some information that might be useful for register allocation
+    /// They are initialized in runOnMachineFunction
+    MachineFrameInfo *MFI;
+    MachineRegisterInfo *MRI;
+    const TargetRegisterInfo *TRI;
+    const TargetInstrInfo *TII;
+    RegisterClassInfo RegClassInfo;
+
+    // TODO: maintain information about live registers
+
+  public:
+    StringRef getPassName() const override { return "Simple Register Allocator"; }
+
+    void getAnalysisUsage(AnalysisUsage &AU) const override {
+      AU.setPreservesCFG();
+      // At -O1/-O2, llc fails to schedule some required passes if this pass
+      // does not preserve these anlyses; these are preserved by recomputing
+      // them at the end of runOnFunction(), you can safely ignore these
+      AU.addRequired<LiveIntervals>();
+      AU.addPreserved<LiveIntervals>();
+      AU.addRequired<SlotIndexes>();
+      AU.addPreserved<SlotIndexes>();
+      MachineFunctionPass::getAnalysisUsage(AU);
+    }
+
+    /// Ask the Machine IR verifier to check some simple properties
+    /// Enabled with the -verify-machineinstrs flag in llc
+    MachineFunctionProperties getRequiredProperties() const override {
+      return MachineFunctionProperties().set(
+          MachineFunctionProperties::Property::NoPHIs);
+    }
+
+    MachineFunctionProperties getSetProperties() const override {
+      return MachineFunctionProperties().set(
+          MachineFunctionProperties::Property::NoVRegs);
+    }
+
+    MachineFunctionProperties getClearedProperties() const override {
+      return MachineFunctionProperties().set(
+        MachineFunctionProperties::Property::IsSSA);
+    }
+
+  private:
+
+    /// Allocate physical register for virtual register operand
+    void allocateOperand(MachineOperand &MO, Register VirtReg, bool is_use) {
+      // TODO: allocate physical register for a virtual register
+    }
+
+    void allocateInstruction(MachineInstr &MI) {
+      // TODO: find and allocate all virtual registers in MI
+    }
+
+    void allocateBasicBlock(MachineBasicBlock &MBB) {
+      // TODO: allocate each instruction
+      // TODO: spill all live registers at the end
+    }
+
+    bool runOnMachineFunction(MachineFunction &MF) override {
+      dbgs() << "simple regalloc running on: " << MF.getName() << "\n";
+
+      outs() << "simple regalloc not implemented\n";
+      abort();
+
+      // Get some useful information about the target
+      MRI = &MF.getRegInfo();
+      const TargetSubtargetInfo &STI = MF.getSubtarget();
+      TRI = STI.getRegisterInfo();
+      TII = STI.getInstrInfo();
+      MFI = &MF.getFrameInfo();
+      MRI->freezeReservedRegs(MF);
+      RegClassInfo.runOnMachineFunction(MF);
+
+      // Allocate each basic block locally
+      for (MachineBasicBlock &MBB : MF) {
+        allocateBasicBlock(MBB);
+      }
+
+      MRI->clearVirtRegs();
+
+      // Recompute the analyses that we marked as preserved above, you can
+      // safely ignore this code
+      SlotIndexes& SI = getAnalysis<SlotIndexes>();
+      SI.releaseMemory();
+      SI.runOnMachineFunction(MF);
+
+      LiveIntervals& LI = getAnalysis<LiveIntervals>();
+      LI.releaseMemory();
+      LI.runOnMachineFunction(MF);
+
+      return true;
+    }
+  };
+}
+
+/// Create the initializer and register the pass
+char RegAllocSimple::ID = 0;
+FunctionPass *llvm::createSimpleRegisterAllocator() { return new RegAllocSimple(); }
+INITIALIZE_PASS(RegAllocSimple, "regallocsimple", "Simple Register Allocator", false, false)
+static RegisterRegAlloc simpleRegAlloc("simple", "simple register allocator", createSimpleRegisterAllocator);
